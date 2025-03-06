167bcffbd89c5ea1b7f018c2db60ba5bd67dbb8c
Lucas Peixoto Gonçalves <aluno35@Lucas-Peixoto.local>
2025-03-06 08:29:06 -0300

    estrutura da historia adicionada

---
+++ semScript/historia/ambientacao.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let texto1_1 = "\n\n Jóias cintilando, champanhe nas taças, risadas estridentes. Estamos na Pinacoteca de São Paulo para o leilão de “Passeio ao Crepúsculo”, de Van Gogh, que pertenceu a Benedito Cavalcanti, grande curador de arte. Como detetive, você sempre teve uma paixão por pinturas. Agora, espera para ver quem dará o maior lance.\n"
+let texto1_2 = "De repente, o alarme de incêndio! O salão explode em caos, com segurança tentando guiar os convidados para fora. Você permanece calmo, mas se pergunta de onde veio o fogo enquanto deixa o museu com os outros. \nFora do salão, o policial Azevedo e o leiloeiro se aproximam."
+let texto1_3 = "\u{001B}[33mPolicial Azevedo:\u{001B}[0mBoa tarde, Detetive Ana Lise. A obra de Van Gogh desapareceu no meio da confusão. Podemos contar com sua ajuda? \nVocê, incomodada pela interrupção, pondera. O sumiço de uma obra tão valiosa te inquieta.\n\n"
+
+let opcao1_1 = "Aceitar ajudar o policial Azevedo."
+let opcao1_2 = "Negar a ajuda e seguir seu caminho"
+
+let texto2_1 = ""
