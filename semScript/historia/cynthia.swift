bfd08481994fab0647bf99fa69f26ff76e465b6d
Lucaspeixotg <Lucaspeixotopro@gmail.com>
2025-03-06 11:42:23 -0300

    cynthia added

---
+++ semScript/historia/cynthia.swift
@@ -0,0 +1,159 @@
+import Foundation
+
+let texto17_1 = "\n\nVocê entra no escritório da casa e encontra Cynthia Cavalcanti sentada em uma poltrona de veludo vinho. Uma xícara de chá repousa em uma escrivaninha à sua frente, intacta. Ela levanta os olhos ao notar sua presença, lhe dirigindo um olhar sério.\n"
+let texto17_2 = "Cynthia: Então, você é a detetive que a polícia chamou? Não imaginei que chegaria tão rápido.\n"
+let texto17_3 = "Você: Isso mesmo, estou investigando o desaparecimento da pintura. Imagino que sua família tenha uma conexão especial com a obra.\n"
+let texto17_4 = "Cynthia: (suspira, passando a mão nos cabelos soltos ondulados) Especial... Meu pai amava aquele quadro. Nunca pensou em vendê-lo, mesmo recebendo muitas propostas de museus e de outros colecionadores. Ele odiaria essa confusão toda, odiaria a possibilidade dessa investigação criar mais intrigas na nossa família. E acho que é por isso que quero que isso tudo acabe logo, então pode fazer perguntas à vontade, detetive.\n"
+
+let opcao17_1 = "Perguntar como era a relação dela com o pai"
+let opcao17_2 = "Perguntar o motivo de terem vendido a obra"
+let opcao17_3 = "Perguntar sobre a relação dela com os irmãos"
+
+
+//opcao 1 do 17
+let texto18_1 = "Você: Parece que seu pai era uma figura importante para você. Como era sua relação com ele?\n"
+let texto18_2 = "Cynthia: (faz uma pausa, pensativa, antes de responder com um sorriso nostálgico) Meu pai era um homem fascinante, apaixonado pelo que fazia. Ele tinha um olhar único para a arte, nenhum detalhe escapava da sua atenção. Eu o admirava muito e nós sempre fomos muito próximos.\n"
+let texto18_3 = "Você: (percebe um tom de hesitação em sua voz) Mas?\n"
+let texto18_4 = "Cynthia: Mas ele também era... Um sonhador. Para ele, a arte vinha sempre em primeiro lugar, e o resto—dinheiro, estabilidade, o futuro—era secundário. Eu sempre fui a mais pé no chão da família, então acabei assumindo um papel mais prático.\n"
+let texto18_5 = "Você: Você cuidava da parte financeira?\n"
+let texto18_6 = "Cynthia: Alguém precisava, não é? Depois que minha mãe faleceu, fui eu quem garantiu que as contas fossem pagas. Meu pai me ensinou a valorizar a arte, mas fui eu quem aprendeu a lidar com a realidade.\n"
+
+let opcao18_1 = "Perguntar se o motivo da venda era pelo dinheiro"
+let opcao18_2 = "Conversar com os outros suspeitos"
+
+//opcao 1 do 18
+let texto19_1 = "Você: Então, isso quer dizer que a venda da obra foi por dinheiro?"
+let texto19_2 = "Cynthia: (solta um suspiro, olhando para você como se já esperasse essa pergunta) Não tínhamos outra escolha. Quando meu pai faleceu, descobrimos que, apesar de toda sua reputação, ele não deixou uma grande herança."
+let texto19_3 = "Você: Ele não acumulou nada?"
+let texto19_4 = "Cynthia: A maior parte do dinheiro foi gasta ao longo dos anos, patrocinando exposições, ajudando artistas, adquirindo peças que ele considerava importantes. Meu pai sempre acreditou que o valor real da arte estava em compartilhá-la, não em guardá-la para si."
+let texto19_5 = "Você: E a pintura era o ativo mais fácil de transformar em dinheiro?"
+let texto19_6 = "Cynthia: Exatamente. Manter um Van Gogh em casa não paga contas, nem a faculdade da minha filha. Eu sei que pode parecer frio, mas foi uma decisão racional."
+
+let opcao19_1 = "Conversar com outros suspeitos"
+
+
+// opcao 2 do 17
+let texto20_1 = "Você: Se a obra era tão valiosa para ele, por que vendê-la?"
+let texto20_2 = "Cynthia: (solta um suspiro, olhando para você como se já esperasse essa pergunta) Porque não tínhamos outra escolha. Meu pai era um grande curador, um apaixonado pela arte, mas... não um homem de negócios. Quando faleceu, descobrimos que, apesar de toda sua reputação, ele não deixou uma grande herança."
+let texto20_3 = "Você: Ele não acumulou nada?"
+let texto20_4 = "Cynthia: A maior parte do dinheiro foi gasta ao longo dos anos, patrocinando exposições, ajudando artistas, adquirindo peças que ele considerava importantes. Meu pai sempre acreditou que o valor real da arte estava em compartilhá-la, não em guardá-la para si."
+let texto20_5 = "Você: E a pintura era o ativo mais fácil de transformar em dinheiro?"
+let texto20_6 = "Cynthia: Exatamente. Manter um Van Gogh em casa não paga contas, nem a faculdade da minha filha. Eu sei que pode parecer frio, mas foi uma decisão racional."
+
+let opcao20_1 = "Perguntar sobre os gastos com a faculdade da filha dela"
+let opcao20_2 = "Perguntar o que os irmãos acham da venda"
+let opcao20_3 = "Conversar com os outros suspeitos"
+
+
+// opcao 1 do 20
+let texto21_1 = "Você: Você mencionou que precisava do dinheiro para a faculdade da sua filha. Imagino que seja um investimento alto."
+let texto21_2 = "Cynthia: Sim, é uma universidade particular renomada. Minha filha tem talento, assim como o meu pai, mas diferente dele, eu quero garantir que ela tenha estabilidade também. Quero que minha filha tenha as ferramentas para fazer o que ama, mas sem passar pelas mesmas incertezas."
+let texto21_3 = "Você: E seu ex-marido, o pai dela, não ajuda financeiramente?"
+let texto21_4 = "Cynthia: (solta uma risada seca, sem humor) Se ajudar significa desaparecer e deixar todas as responsabilidades para mim, então sim, ele \"ajuda\"."
+let texto21_5 = "Você: (refletindo) Então vender a obra era uma forma de garantir que sua filha continuasse os estudos sem problemas?"
+let texto21_6 = "Cynthia: (suspira e encosta no sofá, parecendo cansada, mas resoluta) Exatamente. Eu faria qualquer coisa por ela. Se eu pudesse garantir que o dinheiro do leilão fosse só pra faculdade dela, e não pros ingratos dos meus irmãos, eu faria isso."
+
+let opcao21_1 = "Conversar com outros suspeitos"
+
+// opcao 2 do 20
+let texto22_1 = "Você: E os seus irmãos? Como eles reagiram à decisão de vender a obra?"
+let texto22_2 = "Cynthia: Ah, meus irmãos... cada um com sua visão do mundo."
+let texto22_3 = "Você: O que quer dizer com isso?"
+let texto22_4 = "Cynthia: (seu olhar endurece por um instante, antes de relaxar um pouco e continuar) César nunca ligou para a arte. Ele só enxerga formas de bancar seu estilo de vida. Para ele, vender o quadro era a coisa mais óbvia do mundo. Afinal, o leilão iria significar mais dinheiro pra gastar em festas e apostas."
+let texto22_5 = "Você: E Cristiano?"
+let texto22_6 = "Cynthia: (seu tom muda, ficando mais cuidadoso, quase apreensivo) Cristiano... Bem, ele é diferente. Meu irmão ama arte, mas de um jeito intenso. Ele acredita que algumas peças não são realmente arte, e ele quer que elas deixem de existir, que não sejam valorizadas."
+let texto22_7 = "Você: Quer dizer que ele foi contra a venda?"
+let texto22_8 = "Cynthia: Completamente. Mas não pelas razões normais, de uma pessoa normal. Ele é contra a venda porque ele queria destruir o quadro! Ele já destruiu obras antes. Eu não duvido de nada, não duvido que ele tenha roubado o quadro para queimar ele!"
+
+let opcao22_1 = "Conversar com outros suspeitos"
+
+
+// opcao 3 do 17
+let texto23_1 = "Você: E os seus irmãos? Como eles reagiram à decisão de vender a obra?"
+let texto23_2 = "Cynthia: Ah, meus irmãos... cada um com sua visão do mundo."
+let texto23_3 = "Você: O que quer dizer com isso?"
+let texto23_4 = "Cynthia: (seu olhar endurece por um instante, antes de relaxar um pouco e continuar) César nunca ligou para a arte. Ele só enxerga formas de bancar seu estilo de vida. Para ele, vender o quadro era a coisa mais óbvia do mundo. Afinal, o leilão iria significar mais dinheiro pra gastar em festas e apostas."
+let texto23_5 = "Você: E Cristiano?"
+let texto23_6 = "Cynthia: (seu tom muda, ficando mais cuidadoso, quase apreensivo) Cristiano... Bem, ele é diferente. Ele acredita que algumas peças não são realmente arte, e ele quer que elas deixem de existir, que não sejam valorizadas."
+let texto23_7 = "Você: Quer dizer que ele foi contra a venda?"
+let texto23_8 = "Cynthia: Completamente. Mas não pelas razões de uma pessoa normal. Ele é contra a venda porque ele queria destruir o quadro! Ele já destruiu obras antes. Eu não duvido de nada, não duvido que ele tenha roubado o quadro para queimar ele!"
+
+let opcao23_1 = "Perguntar sobre a relação dela com sua filha"
+let opcao23_2 = "Conversar com outros suspeitos"
+
+
+let texto24_1 = "Você: E Dalila, sua filha? Ela herdou essa conexão com a arte do Benedito?"
+let texto24_2 = "Cynthia: Totalmente. Dalila e meu pai eram inseparáveis. Ele adorava ensiná-la sobre arte, levava ela para exposições, falava sobre grandes mestres com uma paixão que eu nunca vi igual."
+let texto24_3 = "Você: Ela deve ter ficado arrasada quando ele faleceu."
+let texto24_4 = "Cynthia: Muito. Acho que foi a maior perda da vida dela. Dalila sempre foi muito forte, muito decidida, mas a morte do meu pai mexeu com. Ela ficou mais fechada… e, ao mesmo tempo, mais determinada. Sempre teve um senso de justiça muito forte, sabe? Acho que ela sentiu que precisava proteger o legado do avô."
+let texto24_5 = "Você: E o que ela achava de vender esse Van Gogh?"
+let texto24_6 = "Cynthia: (seu semblante fica mais sério, como se não gostasse da insinuação) Ela não gostou, claro. Mas entendeu que era necessário. Dalila é idealista, mas também inteligente. Sabia que o dinheiro da venda seria importante para todos nós e, principalmente, para a faculdade dela. Ela não faria nada para impedir a venda da obra."
+
+let opcao24_1 = "Conversar com outros suspeitos"
+
+
+let texto25_1 = "Você decide que já obteve todas as informações que Cynthia poderia oferecer."
+
+let opcao25_1 = "Conversar com César Cavalcanti"
+let opcao25_2 = "Conversar com Dalila Cavalcanti"
+let opcao25_3 = "Conversar com Cristiano Cavalcanti"
+let opcao25_4 = "Explorar a casa"
+let opcao25_5 = "Apostar suspeito final"
+
+
+// arvore
+
+let dialogo25 = Dialogo(descricao: [texto25_1],
+                        opcoes: nil /* [
+                            Opcao(texto: opcao25_1, continuacao: dialogo25),
+                            Opcao(texto: opcao25_2, continuacao: dialogo25),
+                            Opcao(texto: opcao25_3, continuacao: dialogo25),
+                            Opcao(texto: opcao25_4, continuacao: dialogo25),
+                            Opcao(texto: opcao25_5, continuacao: dialogo25)
+                            ]*/)
+
+let dialogo24 = Dialogo(descricao: [texto24_1, texto24_2, texto24_3, texto24_4, texto24_5],
+                        opcoes: [
+                            Opcao(texto: opcao24_1, continuacao: dialogo25),
+                            ])
+
+let dialogo23 = Dialogo(descricao: [texto23_1, texto23_2, texto23_3, texto23_4, texto23_5, texto23_6, texto23_7, texto23_8],
+                        opcoes: [
+                            Opcao(texto: opcao23_1, continuacao: dialogo24),
+                            Opcao(texto: opcao23_2, continuacao: dialogo25)
+                            ])
+
+let dialogo22 = Dialogo(descricao: [texto22_1, texto22_2, texto22_3, texto22_4, texto22_5, texto22_6, texto22_7, texto22_8],
+                        opcoes: [
+                            Opcao(texto: opcao22_1, continuacao: dialogo25),
+                            ])
+
+let dialogo21 = Dialogo(descricao: [texto21_1, texto21_2, texto21_3, texto21_4, texto21_5, texto21_6],
+                        opcoes: [
+                            Opcao(texto: opcao21_1, continuacao: dialogo25),
+                            ])
+
+let dialogo20 = Dialogo(descricao: [texto20_1, texto20_2, texto20_3, texto20_4, texto20_5, texto20_6],
+                        opcoes: [
+                            Opcao(texto: opcao20_1, continuacao: dialogo21),
+                            Opcao(texto: opcao20_2, continuacao: dialogo22),
+                            Opcao(texto: opcao20_3, continuacao: dialogo25)
+                            ])
+
+let dialogo19 = Dialogo(descricao: [texto19_1, texto19_2, texto19_3, texto19_4, texto19_5, texto19_6],
+                        opcoes: [
+                            Opcao(texto: opcao19_1, continuacao: dialogo25),
+                            ])
+
+let dialogo18 = Dialogo(descricao: [texto18_1, texto18_2, texto18_3, texto18_4, texto18_5, texto18_6],
+                        opcoes: [
+                            Opcao(texto: opcao18_1, continuacao: dialogo19),
+                            Opcao(texto: opcao18_2, continuacao: dialogo25)
+                            ])
+
+let dialogo17 = Dialogo(descricao: [texto17_1, texto17_2, texto17_3, texto17_4],
+                        opcoes: [
+                        Opcao(texto: opcao17_1, continuacao: dialogo18),
+                        Opcao(texto: opcao17_2, continuacao: dialogo20),
+                        Opcao(texto: opcao17_3, continuacao: dialogo23)
+                    ])
---
+++ semScript/historia/finais.swift
@@ -0,0 +1,8 @@
+//
+//  finais.swift
+//  semScript
+//
+//  Created by Aluno 35 on 06/03/25.
+//
+
+import Foundation
