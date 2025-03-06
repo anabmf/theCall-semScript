d0da993b380ff80d8f6cb3b11e32e9d8e43a3a19
Ana Margarida Diniz Silva Borges <164074075+anamargaridaborges@users.noreply.github.com>
2025-03-06 12:03:56 -0300

    ad diálogo cristiano

---
+++ semScript/historia/cristiano.swift
@@ -0,0 +1,90 @@
+import Foundation
+
+let texto26_1 = "Você encontra Cristiano em seu ateliê improvisado dentro da casa, rodeado por telas inacabadas e tintas espalhadas. Ele é um artista polêmico, mais conhecido por ter destruído obras que ele não considerava arte. Ele parece distraído, observando uma pintura com um olhar crítico. Quando percebe sua presença, ele vira lentamente, franzindo a testa como se você fosse uma interrupção inconveniente."
+let texto26_2 = "Cristiano: Então você é a detetive que a polícia chamou para resolver essa palhaçada."
+let texto26_3 = "Você: Se por \"palhaçada\" você quer dizer o roubo do Van Gogh do seu pai, então sim."
+let texto26_4 = "Cristiano: (solta uma risada curta, sarcástica, e gesticula com as mãos manchadas de tinta) Roubo… que palavra dramática. O verdadeiro crime foi terem colocado aquele quadro horroroso à venda, não podemos chamar uma coisa dessas de obra de arte."
+let texto26_5 = "Você: Então você não gostava da obra?"
+let texto26_6 = "Cristiano: (exalta-se, gesticulando mais, como se fosse óbvio) Não consigo entender como uma pintura como \"Passeio ao Crepúsculo\" é tão valiosa. Ela é uma aberração! As pessoas têm que entender que nem tudo que tem o nome Van Gogh é bom. Meu pai era um curador respeitável, mas nunca entendi por que guardou essa tela ridícula. Se fosse por mim, esse quadro teria sido queimado anos atrás."
+
+let opcao26_1 = "Perguntar porque ele era contra a venda do quadro"
+let opcao26_2 = "Perguntar sobre as obras que ele já destruiu"
+let opcao26_3 = "Perguntar sobre a relação dele com sua família"
+
+let texto27_1 = "Você: (inclina-se levemente para frente, observando a reação dele) Se odiava tanto essa pintura, por que era contra a venda?"
+let texto27_2 = "Cristiano: Porque é pior vendê-la! Agora alguém vai pagar milhões por uma obra sem alma e colocá-la em uma parede estéril, sem qualquer significado real. Isso é o que me enoja no mercado de arte! Transformam um pedaço da alma de um artista, em mercadoria para ricos entediados! A arte não é isso, a arte é emoção, é angústia, é tudo que essa obra não representa."
+let texto27_3 = "Você: Mas se você a detestava, não era melhor que ela sumisse da coleção da sua família?"
+let texto27_4 = "Cristiano: (seus olhos brilham com uma intensidade perigosa) Não! Porque agora essa coisa vai ganhar ainda mais valor, ser tratada como se fosse uma joia preciosa! Eu preferia que fosse destruída, mas não pelas mãos de colecionadores. E antes que você pergunte, não, eu não a roubei. Se tivesse feito algo, ela não teria simplesmente desaparecido… teria virado cinzas. E todos saberiam disso, todos iriam ser obrigados a lidar com o fato de que ela não era uma obra de arte."
+
+let opcao27_1 = "Perguntar sobre as obras que ele já destruiu"
+let opcao27_2 = "Conversar com outros suspeitos"
+
+let texto28_1 = "Você: Soube que você já destruiu algumas obras no passado. Pode me contar mais sobre isso?"
+let texto28_2 = "Cristiano: (sorri de canto, como se a pergunta lhe agradasse) Destruir? Pff… Não é bem assim. Eu libertei a arte de sua própria mediocridade. Algumas peças simplesmente não merecem existir."
+let texto28_3 = "Você: E quem decide o que merece existir?"
+let texto28_4 = "Cristiano: O próprio artista, quando é honesto consigo mesmo. Mas quando ele não tem coragem de admitir o erro… alguém tem que fazer isso por ele."
+let texto28_5 = "Você: E \"Passeio ao Crepúsculo\"? Você queria destruir esse quadro?"
+let texto28_6 = "Cristiano: Querer? Sim. Mas eu não sou um ladrão. Se fosse para acabar com aquela aberração, eu teria feito isso na frente de todos, sem me esconder como um rato."
+let texto28_7 = "Você: E como acha que ele foi roubado?"
+let texto28_8 = "Cristiano: (dá de ombros, pegando um pincel e começando a pintar algo sem olhar para você) Provavelmente algum que só quis encher os bolsos, que queria o dinheiro só pra si."
+
+let opcao28_1 = "Conversar com outros suspeitos"
+
+let texto29_1 = "Você: Soube que você já destruiu algumas obras no passado. Pode me contar mais sobre isso?"
+let texto29_2 = "Cristiano: (sorri de canto, como se a pergunta lhe agradasse) Destruir? Pff… Não é bem assim. Eu libertei a arte de sua própria mediocridade. Algumas peças simplesmente não merecem existir."
+let texto29_3 = "Você: E quem decide o que merece existir?"
+let texto29_4  = "Cristiano: O próprio artista, quando é honesto consigo mesmo. Mas quando ele não tem coragem de admitir o erro… alguém tem que fazer isso por ele."
+let texto29_5 = "Você: E \"Passeio ao Crepúsculo\"? Você queria destruir esse quadro?"
+let texto29_6 = "Cristiano: Querer? Sim. Mas eu não sou um ladrão. Se fosse para acabar com aquela aberração, eu teria feito isso na frente de todos, sem me esconder como um rato."
+let texto29_7 = "Você: E como acha que ele foi roubado?"
+let texto29_8 = "Cristiano: (dá de ombros, pegando um pincel e começando a pintar algo sem olhar para você) Provavelmente algum que só quis encher os bolsos, que queria o dinheiro só pra si."
+
+let opcao29_1 = "Perguntar sobre a relação dele com a família"
+let opcao29_2 = "Conversar com outros suspeitos"
+
+let texto30_1 = "Você: (decide testar sua opinião sobre os irmãos, já que ele tem uma visão forte sobre tudo) E seus irmãos? O que achou da decisão deles de vender o quadro?"
+let texto30_2 = "Cristiano: Cynthia faz tudo pelo dinheiro, sempre pensando na próxima conta a pagar. Ela parece um robô, não entendo como ela é sangue do meu sangue. Não me surpreenderia se ela tivesse roubado o quadro pra vender e ficar com tudo pra ela. César… bem, ele nem se importa com arte, só com festas e apostas. E Dalila…"
+let texto30_3 = "Você: (percebe sua hesitação) O que tem Dalila?"
+let texto30_4 = "Cristiano: (estreita os olhos, pensativo por um instante antes de responder) Ela é como eu, mas ela esconde melhor que eu. Ela sim é uma artista, mesmo não tendo entendido ainda como o mundo da arte funciona. Coitada, ela ainda acha que pode consertar o mundo, que tudo é belo e que até o quadro mais hediondo merece ser exposto. Ela não queria que o quadro fosse vendido… ela era próxima do meu pai, cheia de sentimentalismos."
+
+let opcao30_1 = "Conversar com outros suspeitos"
+
+let texto31_1 = "Você: (decide testar sua opinião sobre os irmãos, já que ele tem uma visão forte sobre tudo) E seus irmãos? O que achou da decisão deles de vender o quadro?"
+let texto31_2 = "Cristiano: Cynthia faz tudo pelo dinheiro, sempre pensando na próxima conta a pagar. Ela parece um robô, não entendo como ela é sangue do meu sangue. Não me surpreenderia se ela tivesse roubado o quadro pra vender e ficar com tudo pra ela. César… bem, ele nem se importa com arte, só com festas e apostas. E Dalila…"
+let texto31_3 = "Você: (percebe sua hesitação) O que tem Dalila?"
+let texto31_4 = "Cristiano: (estreita os olhos, pensativo por um instante antes de responder) Ela é como eu, mas ela esconde melhor que eu. Ela sim é uma artista, mesmo não tendo entendido ainda como o mundo da arte funciona. Coitada, ela ainda acha que pode consertar o mundo, que tudo é belo e que até o quadro mais hediondo merece ser exposto. Ela não queria que o quadro fosse vendido… ela era próxima do meu pai, cheia de sentimentalismos."
+
+let opcao31_1 = "Perguntar sobre a relação dele com o pai"
+let opcao31_2 = "Conversar com outros suspeitos"
+
+let texto32_1 = "Você: (mudando de abordagem, buscando entender melhor sua conexão com o pai) Seu pai manteve essa obra na coleção por anos. Você chegou a perguntar a ele por quê?"
+let texto32_2 = "Cristiano: (seu semblante suaviza por um breve momento, mas logo volta à sua expressão intensa) Meu pai… ele entendia a arte como poucos. Mas, às vezes, ele também se apegava demais a certas peças, por razões sentimentais, e não estéticas. Eu nunca aceitei isso. A arte deve ser julgada pelo que é, não pelo que representa para alguém."
+let texto32_3 = "Você: Ele sabia que você odiava essa pintura?"
+let texto32_4 = "Cristiano: (dá uma risada seca, amarga) Sabia. E ria de mim por isso. Dizia que um dia eu aprenderia a ver a beleza nela. Nunca aprendi."
+
+let opcao32_1 = "Conversar com outros suspeitos"
+
+let texto33_1 = "Depois de uma conversa intensa com Cristiano, você decide seguir a investigação."
+
+let opcao33_1 = "Conversar com César Cavalcanti"
+let opcao33_2 = "Conversar com Dalila Cavalcanti"
+let opcao33_3 = "Conversar com Cynthia Cavalcanti"
+let opcao33_4 = "Explorar a casa"
+let opcao33_5 = "Apontar suspeito final"
+
+let dialogo33 = Dialogo(descricao: [texto33_1], opcoes:nil)
+
+let dialogo32 = Dialogo(descricao: [texto32_1, texto32_2, texto32_3, texto32_4], opcoes: [Opcao(texto: opcao32_1, continuacao: dialogo33)])
+
+
+let dialogo31 = Dialogo(descricao: [texto31_1, texto31_2, texto31_3, texto31_4], opcoes: [Opcao(texto: opcao31_1, continuacao: dialogo32), Opcao(texto: opcao31_2, continuacao: dialogo33)])
+
+let dialogo30 = Dialogo(descricao: [texto30_1, texto30_2, texto30_3, texto30_4], opcoes: [Opcao(texto: opcao30_1, continuacao: dialogo33)])
+
+let dialogo29 = Dialogo(descricao: [texto29_1, texto29_2, texto29_3, texto29_4, texto29_5, texto29_6, texto29_7, texto29_8], opcoes: [Opcao(texto: opcao29_1, continuacao: dialogo30), Opcao(texto: opcao29_2, continuacao: dialogo33)])
+
+let dialogo28 = Dialogo(descricao: [texto28_1, texto28_2, texto28_3, texto28_4, texto28_5, texto28_6, texto28_7, texto28_8], opcoes: [Opcao(texto: opcao28_1, continuacao: dialogo33)])
+
+let dialogo27 = Dialogo(descricao: [texto27_1, texto27_2, texto27_3, texto27_4], opcoes: [Opcao(texto: opcao27_1, continuacao: dialogo28), Opcao(texto: opcao27_2, continuacao: dialogo33)])
+
+let dialogo26 = Dialogo(descricao: [texto26_1, texto26_2, texto26_3, texto26_4, texto26_5, texto26_6], opcoes: [Opcao(texto: opcao26_1, continuacao: dialogo27), Opcao(texto: opcao26_2, continuacao: dialogo29), Opcao(texto: opcao26_3, continuacao: dialogo31)])
