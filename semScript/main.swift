451c7e1376c1de692d97d91b9ea7e683c87696c4
Lucaspeixotg <Lucaspeixotopro@gmail.com>
2025-03-07 08:12:04 -0300

    jogar novamente

--- semScript/main.swift
+++ semScript/main.swift
@@ -2,15 +2,17 @@ import Foundation
 // PRISM
 
 
+func printarTitulo() {
+    print("\u{001B}[35m                _____ _                _           _             _   _  /\\/|         ______")
+    print("    /\\         |  __ (_)              | |         | |           | \\ | ||/\\/         |  ____|")
+    print("   /  \\   ___  | |__) | _ __   ___ ___| | __ _  __| | __ _ ___  |  \\| | __ _  ___   | |__   ___  __ _ _   _  ___  ___ ___ _ __ ___")
+    print("  / /\\ \\ / __| |  ___/ | '_ \\ / __/ _ \\ |/ _` |/ _` |/ _` / __| | . ` |/ _` |/ _ \\  |  __| / __|/ _` | | | |/ _ \\/ __/ _ \\ '_ ` _ \\")
+    print(" / ____ \\\\__ \\ | |   | | | | | (_|  __/ | (_| | (_| | (_| \\__ \\ | |\\  | (_| | (_) | | |____\\__ \\ (_| | |_| |  __/ (_|  __/ | | | | |")
+    print("/_/    \\_\\___/ |_|   |_|_| |_|\\___\\___|_|\\__,_|\\__,_|\\__,_|___/ |_| \\_|\\__,_|\\___/  |______|___/\\__, |\\__,_|\\___|\\___\\___|_| |_| |_|")
+    print("                                                                                                   | |")
+    print("                                                                                                   |_|\u{001B}[0m\n\n")
+}
 
-print("\u{001B}[35m                _____ _                _           _             _   _  /\\/|         ______")
-print("    /\\         |  __ (_)              | |         | |           | \\ | ||/\\/         |  ____|")
-print("   /  \\   ___  | |__) | _ __   ___ ___| | __ _  __| | __ _ ___  |  \\| | __ _  ___   | |__   ___  __ _ _   _  ___  ___ ___ _ __ ___")
-print("  / /\\ \\ / __| |  ___/ | '_ \\ / __/ _ \\ |/ _` |/ _` |/ _` / __| | . ` |/ _` |/ _ \\  |  __| / __|/ _` | | | |/ _ \\/ __/ _ \\ '_ ` _ \\")
-print(" / ____ \\\\__ \\ | |   | | | | | (_|  __/ | (_| | (_| | (_| \\__ \\ | |\\  | (_| | (_) | | |____\\__ \\ (_| | |_| |  __/ (_|  __/ | | | | |")
-print("/_/    \\_\\___/ |_|   |_|_| |_|\\___\\___|_|\\__,_|\\__,_|\\__,_|___/ |_| \\_|\\__,_|\\___/  |______|___/\\__, |\\__,_|\\___|\\___\\___|_| |_| |_|")
-print("                                                                                                   | |")
-print("                                                                                                   |_|\u{001B}[0m\n\n")
 
 // Function to validate input
 func inputIntervalo(a: Int, b: Int) -> Int {
@@ -33,18 +35,36 @@ func inputIntervalo(a: Int, b: Int) -> Int {
 
 var dialogoAtual: Dialogo? = dialogo1
 
-while(dialogoAtual != nil) {
-    dialogoAtual!.efeitoColateral()
-    dialogoAtual!.escrever()
+while true {
+    printarTitulo()
+    
+    while dialogoAtual != nil {
+        dialogoAtual!.efeitoColateral()
+        dialogoAtual!.escrever()
 
-    // intervalo fixo para teste
-    if dialogoAtual!.opcoes == nil {
-        break
+        // intervalo fixo para teste
+        if dialogoAtual!.opcoes == nil {
+            break
+        }
+        
+        let escolha = inputIntervalo(a: 1, b: dialogoAtual!.opcoes!.count)
+        dialogoAtual = dialogoAtual!.opcoes![escolha-1].continuacao
     }
     
-    let escolha = inputIntervalo(a: 1, b: dialogoAtual!.opcoes!.count)
-    dialogoAtual = dialogoAtual!.opcoes![escolha-1].continuacao
+    print("\u{001B}[36m-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=- FIM -=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-")
+    print("\n                       Pressione ENTER para jogar novamente.\n")
+    print("-------------------------------------------------------------------------------------\u{001B}[0m")
+    _ = readLine()
+    
+    dialogoAtual = dialogo1
+    
+    EstadoJogo.atualizarPista(pista: .romance, valor: false)
+    EstadoJogo.atualizarPista(pista: .dalila, valor: false)
+    EstadoJogo.atualizarPista(pista: .tinta, valor: false)
+    EstadoJogo.atualizarPista(pista: .carta, valor: false)
+    
 }
+
 
 
 // 
